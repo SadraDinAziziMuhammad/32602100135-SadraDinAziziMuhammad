0394ab91074b70a2815278bf3cc9495486232361
Sadra Din Azizi Muhammad <sadraazizi1305@gmail.com>
2023-07-11 18:05:01 +0700

    Laporan Praktikum

--- app/Views/Sadra_32602100135.php
+++ app/Views/Sadra_32602100135.php
@@ -1,23 +1,21 @@
 <?= $this->extend('layout/32602100135_Sadra') ?>
+
 <?= $this->section('content') ?>
+
 <div class="about">
   <div class="about-us">
     <h1>About Us</h1>
-    <p>An undergraduate student at the
-      Faculty of Industrial Technology,
-      majoring in Informatics Engineering
-      from the Islamic University of
-      Sultan Agung Semarang</p>
+    <p>An undergraduate student at the Faculty of Industrial Technology, majoring in Informatics Engineering from the Islamic University of Sultan Agung Semarang</p>
   </div>
   <div class="about-experience">
-    <h1>experience</h1>
+    <h1>Experience</h1>
     <div class="about-experience-list">
       <table border='1'>
         <tr>
           <th rowspan='3'>2020</th>
           <th colspan='2'>Direct of Photography</th>
           <th rowspan='3'>2021</th>
-          <th colspan='2'>Processing Auido Video 2D & 3D</th>
+          <th colspan='2'>Processing Audio Video 2D & 3D</th>
         </tr>
         <tr>
           <td>Jakarta</td>
@@ -26,73 +24,88 @@
           <td>Indonesia</td>
         </tr>
         <tr>
-          <td colspan='2'>Being Responsible for Placing
-            an Angel in a Short Film</td>
-          <td colspan='2'>Final Value Determinants that
-            process an audio visual, 2D and 3D animation</td>
+          <td colspan='2'>Being Responsible for Placing an Angel in a Short Film</td>
+          <td colspan='2'>Final Value Determinants that process an audio visual, 2D and 3D animation</td>
         </tr>
       </table>
     </div>
   </div>
 </div>
+
 <style>
   /* app/Views/layout/32602100135_Sadra.css */
+  /* app/Views/layout/32602100135_Sadra.css */
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.about-us,
+.about-experience {
+  padding: 20px;
+  text-align: center;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+
+.about-us h1,
+.about-experience h1 {
+  color: #333;
+  margin-bottom: 10px;
+}
+
+.about-us p {
+  font-size: 16px;
+  color: #666;
+}
+
+.about-experience-list table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.about-experience-list th,
+.about-experience-list td {
+  padding: 10px;
+  border: 1px solid #333;
+}
+
+.about-experience-list th {
+  background-color: #f2f2f2;
+  text-align: center;
+  font-weight: bold;
+}
+
+.about-experience-list td {
+  text-align: left;
+  font-size: 14px;
+}
+
+@media (min-width: 600px) {
+  /* Styles for larger screens */
   .about {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
   }
 
   .about-us,
   .about-experience {
-    padding: 20px;
-    text-align: center;
+    flex: 1;
+    margin: 0 10px;
   }
 
-  .about-us h1,
-  .about-experience h1 {
-    color: #333;
-  }
-
-  .about-experience-list table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px;
-  }
-
-  .about-experience-list th,
-  .about-experience-list td {
-    padding: 10px;
-    border: 1px solid #333;
-  }
-
-  .about-experience-list th {
-    background-color: #f2f2f2;
-    text-align: center;
+  .about-us p {
+    font-size: 18px;
   }
 
   .about-experience-list td {
-    text-align: left;
-    font-size: 14px;
+    font-size: 16px;
   }
+}
 
-  @media (min-width: 600px) {
-
-    /* Styles for larger screens */
-    .about {
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: flex-start;
-    }
-
-    .about-us,
-    .about-experience {
-      flex: 1;
-    }
-
-    .about-experience-list td {
-      font-size: 16px;
-    }
-  }
 </style>
+
 <?= $this->endSection() ?>
--- app/Views/layout/32602100135_Sadra.php
+++ app/Views/layout/32602100135_Sadra.php
@@ -3,29 +3,173 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <title>Layouting</title>
+  <style>
+    body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar ul li {
+  margin-bottom: 10px;
+}
+
+.navbar ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.banner {
+  background-color: #f2f2f2;
+  text-align: center;
+  padding: 20px;
+}
+
+.banner .name {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+}
+
+.banner .description {
+  font-size: 16px;
+  color: #666;
+}
+
+@media (min-width: 600px) {
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .navbar ul {
+    display: flex;
+  }
+
+  .navbar ul li {
+    margin-right: 10px;
+    margin-bottom: 0;
+  }
+}
+
+.navbar-toggle {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+}
+
+.icon-bar {
+  background-color: #fff;
+  display: block;
+  height: 3px;
+  margin-bottom: 5px;
+  transition: background-color 0.3s ease-in-out;
+  width: 25px;
+}
+
+.icon-bar:last-child {
+  margin-bottom: 0;
+}
+
+.navbar-toggle.collapsed .icon-bar {
+  background-color: #fff;
+}
+
+.dropdown-menu {
+  background-color: #333;
+}
+
+.dropdown-menu li {
+  padding: 10px;
+}
+
+.dropdown-menu li a {
+  color: #fff;
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+}
+
+.dropdown-menu li a:hover {
+  color: #999;
+}
+
+/* Perubahan warna tampilan */
+body {
+  background-color: #f8f8f8;
+}
+
+.navbar {
+  background-color: #222;
+}
+
+.navbar ul li a {
+  color: #fff;
+}
+
+.banner {
+  background-color: #ddd;
+}
+
+.banner .name {
+  color: #333;
+}
+
+.banner .description {
+  color: #777;
+}
+
+
+  </style>
 </head>
 
 <body>
-  <div class="navbar">
-    <div class="navbar-page">
-      <ul>
-        <li href="#">Home</li>
-        <li href="#">About Us</li>
-        <li href="#">Portofolio</li>
-        <li href="#">Contact</li>
-      </ul>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Contact <span class="caret"></span></a>
+            <ul class="dropdown-menu" style="background-color: black;">
+              <li><a href="#">LinkedInd</a></li>
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Whatsapp</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="navbar-sosmed">
-      <ul>
-        <li>LinkedInd</li>
-        <li>Instagram</li>
-        <li>Facebook</li>
-        <li>Whatsapp</li>
-      </ul>
-    </div>
-  </div>
+  </nav>
   <div class="banner">
     <span class="name">
       Sadra Din Azizi Muhammad
@@ -35,119 +179,6 @@
     </span>
   </div>
   <?= $this->renderSection('content') ?>
-  <style>
-    /* app/Views/layout.css */
-    /* app/Views/layout.css */
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-    }
-
-    .navbar {
-      display: flex;
-      flex-direction: column;
-      background-color: #333;
-      color: #fff;
-      padding: 10px;
-    }
-
-    .navbar ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      flex-direction: column;
-    }
-
-    .navbar ul li {
-      margin-bottom: 10px;
-    }
-
-    .navbar ul li a {
-      color: #fff;
-      text-decoration: none;
-    }
-
-    .navbar-sosmed {
-      display: flex;
-      margin-top: 10px;
-    }
-
-    .navbar-page {
-      display: flex;
-      margin-top: 10px;
-    }
-
-    .banner {
-      background-color: #f2f2f2;
-      text-align: center;
-      padding: 20px;
-    }
-
-    .banner .name {
-      font-size: 20px;
-      font-weight: bold;
-      color: #333;
-    }
-
-    .banner .description {
-      font-size: 14px;
-      color: #666;
-    }
-
-    /* CSS khusus untuk halaman konten */
-    .content {
-      margin-top: 20px;
-      padding: 20px;
-    }
-
-    .content h1 {
-      color: #333;
-    }
-
-    .content p {
-      color: #666;
-    }
-
-    @media (min-width: 600px) {
-
-      /* Styles for larger screens */
-      .navbar {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-      }
-
-      .navbar ul {
-        flex-direction: row;
-      }
-
-      .navbar ul li {
-        margin-right: 10px;
-        margin-bottom: 0;
-      }
-
-      .navbar-sosmed {
-        display: flex;
-        margin-top: 0;
-      }
-
-      .navbar-page {
-        display: flex;
-        margin-top: 0;
-      }
-
-
-      .banner .name {
-        font-size: 24px;
-      }
-
-      .banner .description {
-        font-size: 16px;
-      }
-    }
-  </style>
 </body>
 
 </html>
